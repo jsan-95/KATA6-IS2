ef81e6db02dd12a67ae4c87d4d3438acb91441d8
Yisus95 <Yisus95@192.168.1.132>
2017-11-07 11:11:26 +0000

    Versión 2: Histograma leyendo desde fichero con modelo CIPO

--- src/kata4/main/Kata4.java
+++ src/kata4/main/Kata4.java
@@ -10,16 +10,30 @@ import kata4.view.MailListReader;
 
 public class Kata4 {
 
-    public static void main(String[] args) throws IOException {
-        String fileName = "/Users/Yisus95/NetBeansProjects/Kata4/src/emails.txt";
-        ArrayList <Mail> mailList = MailListReader.read(fileName);
-        Histogram histogram = MailHistogramBuilder.build(mailList);
+    public static void main(String[] args) throws IOException, Exception {
+        Kata4 kata4 = new Kata4();
+        kata4.execute();
+    }
+    
+    private ArrayList <Mail> mailList;
+    private Histogram histogram;
+    
+    private void execute () throws Exception{
+        input();
+        process();    
+        output();
+    }
 
-        
-        HistogramDisplay histoDisplay = new HistogramDisplay(histogram);
-        
-        
-        
-        histoDisplay.execute();
+    private void input() throws IOException {
+        String fileName = "/Users/Yisus95/NetBeansProjects/Kata4/src/emails.txt";
+        mailList = MailListReader.read(fileName);
+    }
+
+    private void process() {
+        histogram = MailHistogramBuilder.build(mailList);
+    }
+
+    private void output() {
+        new HistogramDisplay(histogram).execute();
     }
 }
